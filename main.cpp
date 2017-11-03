081495b5f9c5df485c742c4cf2b680d8b76cbdb1
Link-God <32190920+Link-God@users.noreply.github.com>
2017-11-03 09:26:15 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include <utility>
+#include<sstream>
+#include<string>
+
+using namespace std ;
+
+bool read(int * array, unsigned int size)
+{
+	string mas;
+	unsigned int i = 0 , array_count=0;
+	getline(cin, mas);
+	istringstream stream(mas);
+	for (i; i < size; i++)
+	{
+		if (stream >> array[i])
+		{
+			array_count++;
+		}
+	}
+	if (array_count ==size && stream.eof()) return true;
+	else return false;
+}
+
+void sort (int * array ,unsigned int size)
+{
+	for(unsigned int i = 1 ; i < size ; i++ )
+	{
+		for(unsigned int j = (i-1) ; j >= 0; j-- )
+		{
+			if(array[j+1] < array [j])
+			{
+				swap(array[j+1] , array[j]);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+}
+
+void print(int * array, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+int main () 
+{
+	unsigned int size ; 
+	int * array ;	
+	cin >> size ;
+	cin.get();
+	array = new int [size];
+	if(read(array, size))
+	{ 
+		sort(array, size);
+		print(array, size);
+		cin.get();
+	}
+	else
+	{
+		cout << "fail input ";
+		cin.get();
+	}
+	delete [] array;
+    return 0;
+}
